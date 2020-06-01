057206627e99c48d78c9ccbea534eb272edf7490
mario1968matic <mario1968matic@gmail.com>
2020-06-01 20:07:48 +0200

    Importing Project

---
+++ Palindrome.Test/UnitTest1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+//using ProvjeraPalindroma;
+
+namespace Palindrome.Test
+{
+    [TestFixture]
+
+    public class UnitTest1
+    {
+        ProvjeraPalindroma Palindrom = new ProvjeraPalindroma();
+
+        [TestCase("idu ljudi")]
+        [TestCase("ana")]
+        [TestCase("evolove")]
+        [TestCase("ANA VOLI MILOVANA")]
+        [TestCase("esineženise")]
+        [TestCase("nitko nije lu da spava")]
+
+        //Provjera je li napisan palindrom
+        public void TestMethod1(string input)
+        {
+            bool expected = true;
+            bool actual = Palindrom.Palindrome_Check(input);
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestCase("Sensas team je najbolji")]
+        [TestCase("nitko nije savršen")]
+        [TestCase("netko mi je ukrao auto")]
+
+        //Provjera kada nije napisan palindrom
+        public void TestMethod2(string input)
+        {
+            bool expected = false;
+            bool actual = Palindrom.Palindrome_Check(input);
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(expected, actual);
+        }
+        
+        
+       
+        [TestCase()]
+        [TestCase(" ")]
+        //Provjera kad nije ništa napisano 
+        public void TestMethod3(string input)
+        {
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.ThrowsException<ArgumentException>(() => Palindrom.Palindrome_Check(input));
+        }
+    }
+}
---
+++ Palindrome/ProvjeraPalindroma.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome
+{
+    public class ProvjeraPalindroma
+    {
+        public bool Palindrome_Check(string myString)
+        {
+            int length = myString.Length;
+            for (int i = 0; i < length / 2; i++)
+            {
+                if (myString[i] != myString[length - i - 1])
+                    return false;
+            }
+            return true;
+        }
+    }
+}
